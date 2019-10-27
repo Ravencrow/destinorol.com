1f3392de28f47d14ba4556c08204da97721e5245
Juan Lucha <juanlucha@gmail.com>
2019-10-27 23:37:03 +0100

    Improved social bar and added discord

--- src/components/header.js
+++ src/components/header.js
@@ -51,6 +51,17 @@ const Header = () => {
           }
         }
       }
+      discordImage: file(relativePath: { eq: "discord-logo.png" }) {
+        childImageSharp {
+          fixed(height: 35) {
+            base64
+            width
+            height
+            src
+            srcSet
+          }
+        }
+      }
       twitchImage: file(relativePath: { eq: "twitch-logo.png" }) {
         childImageSharp {
           fixed(height: 25) {
@@ -71,18 +82,19 @@ const Header = () => {
   `)
 
   return (
-    <header style={{ display: 'flex', flexDirection: 'row', maxWidth: 960, margin: '0 auto 1.45rem' }}>
-      <div style={{ display: 'flex', flexDirection: 'row', flex: 1, padding: '1.45rem 1.0875rem' }}>
+    <header style={{ display: 'flex', flexDirection: 'row', maxWidth: 960, margin: '0 auto 1.45rem', flexWrap: 'wrap' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', flex: 2, padding: '1.45rem 1.0875rem' }}>
         <div style={{ width: 70 }}>
           <Img fixed={data.logoImage.childImageSharp.fixed} />
         </div>
-        <h1 style={{ flex: 'auto', margin: 0 }}>{data.site.siteMetadata.title}</h1>
+        <h1 style={{ flex: 'auto', margin: 0, fontSize: '1.9rem', alignItems: 'center', display: 'flex', flexWrap: 'nowrap', whiteSpace: 'nowrap' }}>{data.site.siteMetadata.title}</h1>
       </div>
-      <div style={{display: 'flex', flex: 1, justifyContent: 'flex-end', alignItems: 'center', flexDirection: 'row'}}>
+      <div style={{display: 'flex', flex: 1, justifyContent: 'flex-end', alignItems: 'center', flexDirection: 'row', marginRight: '20px'}}>
         <a href="https://www.twitch.tv/johnfightgaming" target="_blank"><Img style={{marginLeft: 10 }} fixed={data.twitchImage.childImageSharp.fixed} /></a>
         <a href="https://www.youtube.com/user/destinorol/featured" target="_blank"><Img style={{marginLeft: 10 }} fixed={data.youtubeImage.childImageSharp.fixed} /></a>
         <a href="https://mewe.com/profile/5bbfaa18a40f300b57426c24" target="_blank"><Img style={{marginLeft: 10 }} fixed={data.meweImage.childImageSharp.fixed} /></a>
         <a href="https://twitter.com/JohnFightGaming" target="_blank"><Img style={{marginLeft: 10 }} fixed={data.twitterImage.childImageSharp.fixed} /></a>
+        <a href="https://discord.gg/J9epcs" target="_blank"><Img style={{marginLeft: 10 }} fixed={data.discordImage.childImageSharp.fixed} /></a>
       </div>
     </header>
   )
